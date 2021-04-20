e41564a24678221efd7aa17e58ecdf640135e917
wrack <carlosveiga1618@gmail.com>
2021-04-20 10:23:06 -0300

    add main files and initial setup of server

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+// GET = Buscas
+// POST = Criação
+// PUT = Alteração
+// DELETE = Deletar
+// PATCH = Alterar informação específica
+
+const app = express();
+
+// Creating routes 
+
+// app.get("/", (request, response) => {
+//   return response.send("Olá NLW 05")
+// })
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello NLW 05"
+  })
+})
+
+app.post("/", (request, response) => {
+  return response.json({message: "New user saved"})
+} )
+
+app.listen(3333, () => console.log("Server is running in port 3333"))
