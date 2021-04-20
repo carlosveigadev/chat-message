4a90af225473f657fb5f61119fa5187a64728a10
wrack <carlosveiga1618@gmail.com>
2021-04-20 12:13:52 -0300

    add entity setting and configs

---
+++ src/entities/Setting.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from "typeorm";
+
+@Entity("settings")
+class Setting {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+  
+  @Column()
+  chat: boolean;
+  
+  @UpdateDateColumn()
+  updated_at: Date;
+  
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export { Setting }
