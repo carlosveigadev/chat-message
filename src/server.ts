9858a11c71c87174847d6978369ecce0fad4ba2b
wrack <carlosveiga1618@gmail.com>
2021-04-20 12:33:29 -0300

    import routes and use it in the ap

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
-import './database';
+import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
+
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running in port 3333"))
