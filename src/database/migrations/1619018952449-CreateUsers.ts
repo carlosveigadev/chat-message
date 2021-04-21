e3a8aa1585eae643d367d13e07b93826d4a5b828
wrack <carlosveiga1618@gmail.com>
2021-04-21 12:29:23 -0300

    add new users table

---
+++ src/database/migrations/1619018952449-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1619018952449 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
