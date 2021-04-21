f4526e0f9ffbbdd46116a659773878c9bd88f229
wrack <carlosveiga1618@gmail.com>
2021-04-21 14:54:25 -0300

    refactor services constructors

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
@@ -7,9 +8,14 @@ interface ISettingsCreate {
 }
 
 class SettingsService {
-  async create({ chat, username }: ISettingsCreate) {
-    const settingsRepository = getCustomRepository(SettingsRepository);
+  private settingsRepository: Repository<Setting>;
 
+  constructor() {
+    this.settingsRepository =  getCustomRepository(SettingsRepository);
+  }
+  
+
+  async create({ chat, username }: ISettingsCreate) {
     const userAlreadyExists = await settingsRepository.findOne({ username });
 
     if(userAlreadyExists) {
@@ -17,12 +23,12 @@ class SettingsService {
       
     }
   
-    const settings = settingsRepository.create({
+    const settings = this.settingsRepository.create({
       chat,
       username,
     });
   
-    await settingsRepository.save(settings);
+    await this.settingsRepository.save(settings);
 
     return settings;
   }
