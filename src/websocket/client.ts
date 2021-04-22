123dd82cec1eb6be4b40edd0260e2bab7d26f267
wrack <carlosveiga1618@gmail.com>
2021-04-22 12:52:05 -0300

    add messages services to save messages on database

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,14 +1,18 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
+import { MessagesService } from "../services/MessagesService";
+
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionsService();
   const usersService = new UsersService();
+  const messagesService = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
     const { text, email } = params;
+    let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
 
@@ -18,7 +22,11 @@ io.on("connect", (socket) => {
         socket_id,
         user_id: user.id
       });
+
+      user_id = user.id
     } else {
+      user_id = userExists.id;
+
       const connection = await connectionsService.findByUserId(userExists.id); 
 
       if(!connection) {
@@ -31,7 +39,12 @@ io.on("connect", (socket) => {
         await connectionsService.create(connection);
       }
     }
-  })
+
+    await messagesService.create({
+      text,
+      user_id,
+     });
+  });
 });
 
 
