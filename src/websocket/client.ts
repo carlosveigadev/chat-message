7e98da1f30c9a48fc660e5a6d4dbef57c9ce364a
wrack <carlosveiga1618@gmail.com>
2021-04-22 13:07:08 -0300

    enable response chat

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -3,6 +3,10 @@ import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
 import { MessagesService } from "../services/MessagesService";
 
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionsService();
@@ -11,7 +15,7 @@ io.on("connect", (socket) => {
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
-    const { text, email } = params;
+    const { text, email } = params as IParams;
     let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
