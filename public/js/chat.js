3e41721dae9d7a1d77563a9b46e7435d0f183f5e
wrack <carlosveiga1618@gmail.com>
2021-04-22 11:29:41 -0300

    add config to show chat in support

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,13 @@
 document.querySelector("#start_chat").addEventListener("click", (event) => {
   const socket = io();
+
+  const chat_help = document.getElementById("chat_help");
+  chat_help.style.display = "none";
+
+  const chat_in_support = document.getElementById("chat_in_support");
+  chat_in_support.style.display = "block";
+
+  const email = document.getElementById("email").value;
+  const text = document.getElementById("txt_help").value;
+
 });
