3c4b46fc542dd272a9c52223a63c756f3436ab47
wrack <carlosveiga1618@gmail.com>
2021-04-22 11:07:22 -0300

    add io connection and change app.listen to http.listen

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,12 @@ const app = express();
 const http = createServer(app);
 const io = new Server(http);
 
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id)
+})
+
 app.use(express.json());
 
 app.use(routes); 
 
-app.listen(3333, () => console.log("Server is running on port 3333"))
+http.listen(3333, () => console.log("Server is running on port 3333"))
