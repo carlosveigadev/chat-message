6cb8b08e763b38eb0443a89fe05bcdfedd7a5d01
wrack <carlosveiga1618@gmail.com>
2021-04-26 18:02:26 -0300

    add listner to when new user connects

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -50,7 +50,11 @@ io.on("connect", (socket) => {
      });
 
      const allMessages = await messagesService.listByUser(user_id);
+
      socket.emit("client_list_all_messages", allMessages); 
+
+     const allUsers = await connectionsService.findAllWithoutAdmin();
+     io.emit("admin_list_all_users", allUsers);
   });
 
   socket.on("client_send_to_admin", async params => {
