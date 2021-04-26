e2758b597b32c57c78b78b6766e3ef9cdab6b16b
wrack <carlosveiga1618@gmail.com>
2021-04-26 14:26:41 -0300

    add findallwithoutadmin function to connectionsservice

---
+++ src/websocket/admin.ts
@@ -0,0 +1,8 @@
+import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+
+io.on("connect", async (socket) => {
+  const connectionsService = new ConnectionsService(); 
+
+  const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+})
