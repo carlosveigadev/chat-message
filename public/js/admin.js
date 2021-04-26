c59d2e260bde7cf70de6f46137a6f31855e6b0ea
wrack <carlosveiga1618@gmail.com>
2021-04-26 14:57:03 -0300

    add evento to sent a list of the messages from the user

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,8 @@
 const socket = io();
+let connectionsUsers = [];
 
 socket.on("admin_list_all_users", connections => {
+  connectionsUsers = connections;
   document.getElementById("list_users").innerHTML = "";
 
   let template = document.getElementById("template").innerHTML;
@@ -14,3 +16,25 @@ socket.on("admin_list_all_users", connections => {
     document.getElementById("list_users").innerHTML += rendered;
   })
 })
+
+function call(id) {
+
+  const user = connectionsUsers.find(connection => connection.socket_id === id);
+
+  const template = document.getElementById("admin_template").innerHTML;
+
+  const rendered = Mustache.render(template, {
+    email: user.email,
+    id: user.user_id,
+  })
+
+  document.getElementById("supports").innerHTML += rendered;
+
+  const params = {
+    user_id: user.user_id,
+  }
+
+  socket.emit("admin_list_messages_by_user", params, messages => {
+    console.log('message', messages)
+  })
+}
