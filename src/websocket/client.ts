ebaa2697f12bfba3fac7cafdc7d85bd6c80db7e8
wrack <carlosveiga1618@gmail.com>
2021-04-26 17:47:43 -0300

    finish creating the admin message receiver socket event

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -2,6 +2,7 @@ import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
 import { MessagesService } from "../services/MessagesService";
+import { Socket } from "socket.io";
 
 interface IParams {
   text: string;
@@ -55,13 +56,20 @@ io.on("connect", (socket) => {
   socket.on("client_send_to_admin", async params => {
     const { text, socket_admin_id } = params;
 
-    const { user_id } = await connectionsService.findBySocketId(socket.id);
+    const socket_id = socket.id;
+
+    const { user_id } = await connectionsService.findBySocketId(socket_id);
 
     const message = await messagesService.create({
       text,
       user_id,
+    });
+
+    io.to(socket_admin_id).emit("admin_receive_message", {
+      message,
+      socket_id,
     })
-  })
+  });
 });
 
 
