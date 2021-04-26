15130ccd9ecccd2fdd3cf03bdc86f727b5a3bfd8
wrack <carlosveiga1618@gmail.com>
2021-04-26 14:27:43 -0300

    add event emit to sento to admins

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -5,4 +5,6 @@ io.on("connect", async (socket) => {
   const connectionsService = new ConnectionsService(); 
 
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+
+  io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
 })
