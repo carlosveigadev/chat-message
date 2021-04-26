55007ccceeec6c4d17b6315126c5bb0160816519
wrack <carlosveiga1618@gmail.com>
2021-04-26 17:17:41 -0300

    add events to send message to client

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -17,4 +17,22 @@ io.on("connect", async (socket) => {
 
     callback(allMessages);
   })
+
+  socket.on("admin_send_message", async params => {
+    const { text, user_id } = params;
+
+    await messagesService.create({
+      text,
+      user_id,
+      admin_id: socket.id
+    });
+
+    const { socket_id } = await connectionsService.findByUserId(user_id);
+
+    io.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id,
+    })
+
+  });
 })
